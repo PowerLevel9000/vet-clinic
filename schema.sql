b1e5c9fe220b699f2216a2efb8117d3eafce8221
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-07 18:08:22 +0530

    Species_id and Owener column in parent table

--- schema.sql
+++ schema.sql
@@ -30,9 +30,8 @@ CREATE TABLE animals (
   neutered BOOLEAN NOT NULL,
   weight_kg REAL NOT NULL
 );
+
 -- ************************************************************************* line COMMENTS ARE EXPECTED OUTPUTS **************************************************************
-
-
 -- day-2 TABLE alter
 ALTER TABLE
   animals
@@ -82,3 +81,55 @@ CREATE TABLE species (
 --  name   | character varying(100) |           | not null |
 -- Indexes:
 --     "species_pkey" PRIMARY KEY, btree (id)
+-- drop species from animals
+BEGIN;
+
+-- BEGIN
+ALTER TABLE
+  animals DROP COLUMN species;
+
+ALTER TABLE
+  SAVEPOINT drop_species;
+
+-- SAVEPOINT
+ALTER TABLE
+  animals
+ADD
+  COLUMN species_id int,
+ADD
+  COLUMN owner_id int;
+
+-- ALTER TABLE
+SAVEPOINT species_id_and_owner_id;
+
+ALTER TABLE
+  animals
+ADD
+  CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species(id),
+ADD
+  CONSTRAINT fk_species_id FOREIGN KEY (species_id) REFERENCES species(id);
+
+-- ALTER TABLE
+commit;
+
+-- COMMIT
+/*view the table */
+
+SELECT
+  *
+FROM
+  animals;
+
+--  id |    name    | date_of_birth | escape_attempts | neutered | weight_kg | species_id | owner_id
+-- ----+------------+---------------+-----------------+----------+-----------+------------+----------
+--   6 | Plantmon   | 2021-11-15    |               2 | t        |       5.7 |            |
+--   8 | Angemon    | 2005-06-12    |               1 | t        |        45 |            |
+--   5 | Charmander | 2020-02-08    |               0 | f        |        11 |            |
+--   7 | Squirtle   | 1993-04-02    |               3 | f        |     12.13 |            |
+--   1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 |            |
+--   2 | Gabumon    | 2018-11-15    |               2 | t        |         8 |            |
+--   4 | Devimon    | 2017-05-12    |               5 | t        |        11 |            |
+--   9 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 |            |
+--   3 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 |            |
+--  10 | Blossom    | 1998-10-13    |               3 | t        |        17 |            |
+-- (10 rows)
